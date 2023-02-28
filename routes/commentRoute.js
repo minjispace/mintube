994274d87c38abb72eef7c986ac75a7ee69ebd71
minji <321963741@naver.com>
2023-02-28 17:20:43 +0900

    admin authorize middleware added

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import {createComment, deleteComment, getAllComments, updateComment} from '../controllers/commentController.js';
-import {authenticateUser} from '../middlewares/index.js';
+import {authenticateUser, authorizePermissionsForOnlyAdmin} from '../middlewares/index.js';
 // ------------------------------------------
 
 // router
 const router = express.Router();
 
-router.route('/').get(getAllComments).post(authenticateUser, createComment);
+router.route('/').get(getAllComments).post(authenticateUser, authorizePermissionsForOnlyAdmin('ADMIN'), createComment);
 
-router.route('/:id').patch(authenticateUser, updateComment).delete(authenticateUser, deleteComment);
+router.route('/:id').patch(authenticateUser, authorizePermissionsForOnlyAdmin('ADMIN'), updateComment).delete(authenticateUser, authorizePermissionsForOnlyAdmin('ADMIN'), deleteComment);
 
 //  export router
 export default router;
