290ddc94ab2a4a911f7228ce25ec024e640dfe26
minji <321963741@naver.com>
2023-03-03 12:34:51 +0900

    header Ui 완성

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,5 +1,15 @@
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className=" fixed left-0 right-0 bottom-0 w-full p-4 bg-white rounded-t-lg   md:p-6 dark:bg-gray-800">
+      <span className="text-sm text-gray-500 sm:text-center  dark:text-gray-400">
+        © 2023{' '}
+        <a href="/" className="hover:underline">
+          Mintube
+        </a>
+        . All Rights Reserved.
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/components/Header.js
+++ client/components/Header.js
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <nav class="justify-between px-4 py-3 text-gray-700 border border-gray-200 rounded-b-xl  sm:flex sm:px-5 bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
+    <nav className="justify-between px-4 py-3 text-gray-700 border border-gray-200 rounded-b-xl  sm:flex sm:px-5 bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
       {/*  title */}
-      <a href="#" class="ml-1 text-xl font-medium tracking-widest text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
+      <a href="/" className="ml-1 text-xl font-medium tracking-widest text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">
         Minjitube
       </a>
 
@@ -10,7 +10,7 @@ const Header = () => {
       <button
         id="dropdownDefault"
         data-dropdown-toggle="dropdown"
-        class="inline-flex items-center px-3 py-2 text-sm font-normal text-center text-gray-600 bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-300 dark:focus:ring-gray-700"
+        className="inline-flex items-center px-3 py-2 text-sm font-normal text-center text-gray-600 bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-300 dark:focus:ring-gray-700"
       >
         Logout
       </button>
