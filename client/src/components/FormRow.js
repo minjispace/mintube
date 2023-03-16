6f6e75156769a730c3a0718eccf0f0c190d55a09
minji <321963741@naver.com>
2023-03-16 15:31:39 +0900

    status에따른 placeholder 추가

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -1,4 +1,8 @@
-const FormRow = ({type, name, value, onChange}) => {
+import {useSession} from "next-auth/react";
+
+const FormRow = ({status, type, name, value, onChange}) => {
+  console.log({name, value}, "@@");
+  const {data: session} = useSession();
   return (
     <>
       <label htmlFor={name} className="mb-2 text-l font-medium text-white ">
@@ -7,6 +11,7 @@ const FormRow = ({type, name, value, onChange}) => {
       <input
         type={type}
         value={value}
+        placeholder={status === "updateName" ? session?.user?.name : null}
         name={name}
         onChange={onChange}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 tracking-wider my-2 "
