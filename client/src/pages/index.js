839be9b09b7e0282759d29934abb570360107c88
minji <321963741@naver.com>
2023-03-16 15:30:11 +0900

    delte useless thing

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,6 @@
-import {useSession} from "next-auth/react";
-import {Header, ListProduct, SearchForm} from "../components";
+import {ListProduct, SearchForm} from "../components";
 
 export default function Home() {
-  const {data: session} = useSession();
-
   return (
     <div className="bg-gray-900 w-full h-screen">
       <SearchForm />
