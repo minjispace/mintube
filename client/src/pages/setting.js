9e87f9422766e99eb0ced3b005ad02b12bf77f69
minji <321963741@naver.com>
2023-03-16 15:29:14 +0900

    setting component setup

---
+++ client/src/pages/setting.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function setting() {
+  return (
+    <div className="bg-gray-900 h-screen pt-10">
+      <Link
+        className="py-2.5 px-5 mx-3  mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        href="/user/name"
+      >
+        Update username
+      </Link>
+
+      <Link
+        className="py-2.5 px-5 mx-3  mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        href="/forgot/user/password"
+      >
+        Forgot Password
+      </Link>
+    </div>
+  );
+}
