aed88ae4414a60eb0d4ad590b46b6a5cd029c215
minji <321963741@naver.com>
2023-03-16 15:27:13 +0900

    next.config 변경

--- client/next.config.js
+++ client/next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   // async rewrites() {
   //   return [
   //     {
-  //       source: '/api/:path*',
-  //       destination: 'http://localhost:5000/api/:path*',
+  //       source: "/",
+  //       destination: "http://localhost:5000/",
   //     },
   //   ];
   // },
