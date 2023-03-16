daef72d17c37d6858d88610cc8d30c5114a310af
minji <321963741@naver.com>
2023-03-16 15:29:57 +0900

    login,logout에 따른 header 로직 변경

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,22 @@
+import {useQuery} from "@tanstack/react-query";
 import {signOut, useSession} from "next-auth/react";
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {toast} from "react-hot-toast";
 import {logoutUserData} from "../utils/axios";
 // ---------------------------------------------------------
 const Header = () => {
   const {data: session} = useSession();
   const router = useRouter();
+
+  //  react-query logout
+  const {refetch} = useQuery({
+    queryKey: ["logoutUser"],
+    queryFn: logoutUserData,
+    onError: (error) => toast.error(error?.response?.data?.msg),
+    refetchOnWindowFocus: false,
+    enabled: false,
+  });
 
   return (
     <nav className="flex justify-between items-center px-4 py-5 text-gray-300 border  sm:flex sm:px-5  bg-gray-800 border-gray-800" aria-label="Breadcrumb">
@@ -24,34 +35,34 @@ const Header = () => {
         <div className="flex items-center">
           <h5 className="mx-3">{session.user.name}님</h5>
           <button
-            id="dropdownDefault"
-            data-dropdown-toggle="dropdown"
+            type="button"
             className="inline-flex items-center px-3 py-2 text-sm font-normal text-center text-gray-600 bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-300 dark:focus:ring-gray-700"
-            onClick={() => signOut("logout")}
+            onClick={() => {
+              refetch();
+              signOut();
+            }}
           >
             Logout
           </button>
 
           <Link
             className="py-2.5 px-5 mx-3  mr-2  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-            href="/forgot/user/password"
+            href="/setting"
           >
-            Reset Password
+            Setting
           </Link>
         </div>
       ) : (
         <div className="flex items-center">
           <button
-            id="dropdownDefault"
-            data-dropdown-toggle="dropdown"
+            type="button"
             className="inline-flex items-center px-3 py-2 mx-3 text-sm font-normal text-center text-gray-600 bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-300 dark:focus:ring-gray-700"
             onClick={() => router.push("/login")}
           >
             Login
           </button>
           <button
-            id="dropdownDefault"
-            data-dropdown-toggle="dropdown"
+            type="button"
             className="inline-flex items-center px-3 py-2 text-sm font-normal text-center text-gray-600 bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-300 dark:focus:ring-gray-700"
             onClick={() => router.push("/register")}
           >
