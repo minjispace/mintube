108c65737ab2d997c08e409cb115fb7f970c0fbf
minji <321963741@naver.com>
2023-03-16 15:27:52 +0900

    next-auth 로직 변경 및 쿠키 추가

--- client/src/pages/api/auth/[...nextauth].js
+++ client/src/pages/api/auth/[...nextauth].js
@@ -1,55 +1,67 @@
-import {json} from "express";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import {loginUserData} from "../../../utils/axios";
 
-export const nextOption = {
-  providers: [
-    // login
-    CredentialsProvider({
-      id: "login",
-      name: "login",
-      credentials: {
-        email: {label: "email", type: "email", placeholder: "email"},
-        password: {label: "Password", type: "password"},
-      },
+export const nextOption = (_, res) => {
+  return {
+    providers: [
+      // login
+      CredentialsProvider({
+        id: "login",
+        name: "login",
+        credentials: {
+          email: {label: "email", type: "email", placeholder: "email"},
+          password: {label: "Password", type: "password"},
+        },
 
-      async authorize(credentials, req) {
-        const {email, password} = credentials;
+        async authorize(credentials, _) {
+          const {email, password} = credentials;
+          try {
+            //  react-query api 요청
+            const response = await loginUserData({email, password});
+            const user = response.data.user;
 
-        try {
-          const {
-            data: {user},
-          } = await loginUserData({email, password});
-          return user;
-        } catch (error) {
-          const errorMsg = error.response.data.msg;
-          throw new Error(errorMsg);
-        }
+            // cookie setting
+            const responseCookie = response.headers["set-cookie"];
+            res.setHeader("Set-Cookie", responseCookie);
+
+            //  return user
+            if (user) {
+              return user;
+            } else {
+              return null;
+            }
+          } catch (error) {
+            const errorMsg = error.response.data.msg;
+            console.log(error, "nextauth error");
+            throw new Error(errorMsg);
+          }
+        },
+      }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+      signIn: "/login",
+      signOut: "/",
+      error: "/login",
+    },
+    callbacks: {
+      async signIn({user, account, profile, email, credentials}) {
+        return true;
       },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/login",
-    signOut: "/",
-    error: "/login",
-  },
-  callbacks: {
-    async signIn({user, account, profile, email, credentials}) {
-      console.log({user, account, profile, email, credentials}), "@@@";
-      return true;
+      async redirect({url, baseUrl}) {
+        return baseUrl;
+      },
+      async session({session, user, token}) {
+        return session;
+      },
+      async jwt({token, user, account, profile, isNewUser}) {
+        return token;
+      },
     },
-    async redirect({url, baseUrl}) {
-      return baseUrl;
-    },
-    async session({session, user, token}) {
-      return session;
-    },
-    async jwt({token, user, account, profile, isNewUser}) {
-      return token;
-    },
-  },
+  };
 };
 
-export default NextAuth(nextOption);
+export default (req, res) => {
+  return NextAuth(req, res, nextOption(req, res));
+};
