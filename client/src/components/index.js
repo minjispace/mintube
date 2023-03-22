407f0dfdefc76088a33d2ea6ac692f9e4af6aeb4
minji <321963741@naver.com>
2023-03-22 15:34:54 +0900

    delete useless component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,5 @@
 import Header from "./Header";
 import ListProduct from "./ListProduct";
 import FormRow from "./FormRow";
-import SearchForm from "./SearchForm";
 
-export {Header, ListProduct, FormRow, SearchForm};
+export {Header, ListProduct, FormRow};
