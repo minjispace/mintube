fca516be2565976d1ed747495878a1139d7a7b1c
minji <321963741@naver.com>
2023-03-22 16:08:53 +0900

    delete, edit 진짜 비디오 올린 유저한테만 보여주기

--- client/src/components/ListProduct.js
+++ client/src/components/ListProduct.js
@@ -1,9 +1,12 @@
 import {useQuery} from "@tanstack/react-query";
 import Link from "next/link";
+import {useRouter} from "next/router";
 import {toast} from "react-hot-toast";
 import {getAllVideosData} from "../utils/axios/videoAxios";
 
-const ListProduct = () => {
+const ListProduct = ({realUser}) => {
+  const router = useRouter();
+
   //  react-query forgotPassword 요청
   const {data} = useQuery({
     queryKey: ["getAllVideos"],
@@ -11,17 +14,34 @@ const ListProduct = () => {
     onError: (error) => toast.error(error?.response?.data?.msg),
   });
 
+  const deleteVideo = () => {
+    console.log("delete video");
+  };
+
+  const editVideo = () => {
+    console.log("edit video");
+  };
+
   if (!data) return <h1>no uploded video</h1>;
   return (
     <div className="text-white p-5 ">
       <p>{data?.data?.videoCount}개의 동영상</p>
       <ul>
         {data?.data?.videos?.map((item) => {
-          const {id, title, createdAt, description, fileUrl, user} = item;
+          console.log(item, "item");
+          const {
+            id,
+            title,
+            createdAt,
+            description,
+            fileUrl,
+            user: {email, name},
+          } = item;
           return (
             <li className=" p-5 my-5 border-2 rounded-2xl" key={id}>
               <div className="text-xl my-3">{title}</div>
               <p className="mb-5">{description}</p>
+              {/* see video button */}
               <Link
                 className="py-2.5 px-5   text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                 href={fileUrl}
@@ -29,12 +49,34 @@ const ListProduct = () => {
               >
                 see video
               </Link>
-              <Link
+
+              {/*  see detail button */}
+              <button
                 className="py-2.5 px-5 mx-3  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                href={`/video/${id}`}
+                onClick={() => router.push(`/video/${id}`)}
               >
                 see detail
-              </Link>
+              </button>
+
+              {realUser?.email === email && realUser?.name === name && (
+                <>
+                  {/*  delete video button */}
+                  <button
+                    className="inline-flex items-center px-3 py-2 text-sm font-normal text-center text-gray-600 bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-300 dark:focus:ring-gray-700"
+                    onClick={deleteVideo}
+                  >
+                    delete video
+                  </button>
+
+                  {/*  edit video button */}
+                  <button
+                    className="inline-flex items-center px-3 py-2 mx-3 text-sm font-normal text-center text-gray-600 bg-gray-200 rounded-lg hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-300 dark:focus:ring-gray-700"
+                    onClick={editVideo}
+                  >
+                    edit video
+                  </button>
+                </>
+              )}
             </li>
           );
         })}
