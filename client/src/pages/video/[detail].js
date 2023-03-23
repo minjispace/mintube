1901ebb8d4d4f4b26e70056563e032f3fa828518
minji <321963741@naver.com>
2023-03-23 15:28:17 +0900

    detail page에 comment 붙이기

--- client/src/pages/video/[detail].js
+++ client/src/pages/video/[detail].js
@@ -23,12 +23,12 @@ export const getServerSideProps = async (context) => {
 export default function detailVideo(props) {
   const {videoData} = props;
 
-  if (typeof videoData === String) return toast.error(videoData);
+  console.log(videoData, "video");
 
   return (
-    <div className="bg-gray-900 w-full h-screen text-white grid justify-center pt-20">
+    <div className="bg-gray-900 w-full h-screen text-white  pt-20 px-20">
       <ul className="pt-10">
-        <li className=" p-5  border-2 rounded-2xl">
+        <li className=" ">
           <div className="text-xl my-3">Title : {videoData?.title}</div>
           <p className="mb-5">Description : {videoData?.description}</p>
           <video controls width="400">
@@ -38,6 +38,24 @@ export default function detailVideo(props) {
           {/* see video button */}
         </li>
       </ul>
+
+      <div>
+        {videoData?.comment?.map((item) => {
+          const {
+            id: commentId,
+            message,
+            createdAt,
+            user: {name: userName},
+          } = item;
+          return (
+            <ul className="border-2 rounded-2xl background-blue-400 p-5 w-2/6" key={commentId}>
+              <div className="text-2xl">{userName}</div>
+              <li>message : {message}</li>
+              <p>createdAt.{createdAt}</p>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
