65c8ca79e7a86af07cd3d33ef8a16b3aae1fca71
minji <321963741@naver.com>
2023-03-23 11:25:29 +0900

    margin 추가

--- client/src/components/ListProduct.js
+++ client/src/components/ListProduct.js
@@ -77,7 +77,7 @@ const ListProduct = ({realUser}) => {
 
               {/*  see detail button */}
               <button
-                className="py-2.5 px-5 mx-3  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                className="py-2.5 px-5 mx-3 my-3  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                 onClick={() => {
                   router.push({
                     pathname: `/video/detail`,
