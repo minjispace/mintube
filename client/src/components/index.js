cc342e7e94e9b44769cc927829a275b057ca87b1
minji <321963741@naver.com>
2023-03-23 17:55:18 +0900

    create component이름변경

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import ListProduct from "./ListProduct";
 import FormRow from "./FormRow";
+import CreateComment from "./CreateComment";
 
-export {Header, ListProduct, FormRow};
+export {Header, ListProduct, FormRow, CreateComment};
